a5e0448b4164828b96a8fe55e6234889b2592cbb
Mikheil007 <epistula.am@gmail.com>
2022-10-08 19:11:03 +0300

    did task 1

---
+++ homework1.py
@@ -0,0 +1,11 @@
+# Напишите программу, которая принимает на вход цифру,
+# обозначающую день недели, и проверяет, является ли этот день выходным.
+
+num = int(input('enter number'))
+if num == 6 or num == 7:
+    print('weekend')
+elif num < 1 or num > 7:
+    print('you entered the wrong day of the week')
+elif num > 1 and num < 6:
+    print('not weekend')
+
