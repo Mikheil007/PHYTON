faf7d43e94a5707b1418fbc8b4c772c61ae44916
Mikheil007 <epistula.am@gmail.com>
2022-10-11 19:13:00 +0300

    did task 2

--- homework1.py
+++ homework1.py
@@ -1,13 +1,75 @@
 # Напишите программу, которая принимает на вход цифру,
 # обозначающую день недели, и проверяет, является ли этот день выходным.
 
-num = int(input('enter number'))
-if num == 6 or num == 7:
-    print('weekend')
-elif num < 1 or num > 7:
-    print('you entered the wrong day of the week')
-elif num > 1 and num < 6:
-    print('not weekend')
+# num = int(input('enter number'))
+# if num == 6 or num == 7:
+#     print('weekend')
+# elif num < 1 or num > 7:
+#     print('you entered the wrong day of the week')
+# elif num > 1 and num < 6:
+#     print('not weekend')
 
 
+# Вбиваем с клавиатуры целое число N,
+# а затем выводим на экран все числа от 1 до N
+# n = int(input('enter n: '))
+# i = 1
+# while i < n:
+#     print(i)
+#     i = i + 1
+# else:
+#     print('Цикл окончен, i =', i)
+
+# print('1', '2', '3',   sep='*')
+# print('1', '2', '3',   sep='**')
+# print('1', '2', '3', '4', end='@')
+# print('1', '2', '3', '4', end='@@')
+
+# print('a', 'b', 'c', sep='\n', end='finish')
+# print('d', 'e', 'f', sep='**', end='^__^')
+# print('g', 'h', 'i', sep='+', end='%')
+# print('j', 'k', 'l', sep='-', end='\n')
+# print('m', 'n', 'o', sep='/', end='22')
+
+
+# print('a', 'b', 'c')
+# print('a', 'b', 'c', sep='', end='')
+
+# print('31', '12', '2019', sep='-')
+
+# print('Mercury', 'Venus', sep='*', end='!')
+# print('Mars', 'Jupiter', sep='**', end='?')
+
+# print('a', 'b', 'c', sep='*')
+# print('d', 'e', 'f', sep='**', end='')
+# print('g', 'h', 'i', sep='+', end='%')
+# print('j', 'k', 'l', sep='-', end='\n')
+# print('m', 'n', 'o', sep='/', end='!')
+# print('p', 'q', 'r', sep='1', end='%')
+# print('s', 't', 'u', sep='&', end='\n')
+# print('v', 'w', 'x', sep='%')
+# print('y', 'z', sep='/', end='!')
+
+
+
+# sep = input()
+# a = input()
+# b = input()
+# c = input()
+# print(a, b, c, sep=sep)
+
+# variable_name = input()
+# print('Вы ввели текст:', variable_name)
+
+# name, surname = input(('enter name')), input(('enter surname'))
+# print('Имя:', name, 'Фамилия:', surname)
+
+# name1 = 'Timur'
+# name2 = 'Gvido'
+# name1, name2 = name2, name1
+# print(name1, name2)
+
+# language = 'Python'
+# language = 'Pascal'
+# print(language)
 
